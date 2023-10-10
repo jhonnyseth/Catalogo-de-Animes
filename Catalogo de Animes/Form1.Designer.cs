5bfc424454dea16f9f5c27fbcceef0ec720e323d
jhonnyseth <jhonatan.mallet@gmail.com>
2023-10-10 03:02:19 -0300

    Adicionar arquivos de projeto.

---
+++ Catalogo de Animes/Form1.Designer.cs
@@ -0,0 +1,237 @@
+﻿namespace Catalogo_de_Animes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btndelete = new System.Windows.Forms.Button();
+            this.txtanime = new System.Windows.Forms.TextBox();
+            this.txtepisodio = new System.Windows.Forms.TextBox();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.boxgenero = new System.Windows.Forms.ComboBox();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.BackColor = System.Drawing.Color.Green;
+            this.btnAdd.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnAdd.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnAdd.Font = new System.Drawing.Font("Monotype Corsiva", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.btnAdd.Location = new System.Drawing.Point(27, 336);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(95, 30);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "ADD";
+            this.btnAdd.UseVisualStyleBackColor = false;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.BackColor = System.Drawing.Color.Green;
+            this.btnEdit.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnEdit.Font = new System.Drawing.Font("Monotype Corsiva", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.btnEdit.Location = new System.Drawing.Point(136, 336);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(86, 30);
+            this.btnEdit.TabIndex = 2;
+            this.btnEdit.Text = "EDIT";
+            this.btnEdit.UseVisualStyleBackColor = false;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
+            // btndelete
+            // 
+            this.btndelete.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.btndelete.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btndelete.Font = new System.Drawing.Font("Monotype Corsiva", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.btndelete.Location = new System.Drawing.Point(27, 372);
+            this.btndelete.Name = "btndelete";
+            this.btndelete.Size = new System.Drawing.Size(195, 27);
+            this.btndelete.TabIndex = 3;
+            this.btndelete.Text = "DELETE";
+            this.btndelete.UseVisualStyleBackColor = false;
+            this.btndelete.Click += new System.EventHandler(this.btndelete_Click);
+            // 
+            // txtanime
+            // 
+            this.txtanime.Location = new System.Drawing.Point(103, 173);
+            this.txtanime.Name = "txtanime";
+            this.txtanime.Size = new System.Drawing.Size(119, 20);
+            this.txtanime.TabIndex = 5;
+            // 
+            // txtepisodio
+            // 
+            this.txtepisodio.Location = new System.Drawing.Point(103, 285);
+            this.txtepisodio.Name = "txtepisodio";
+            this.txtepisodio.Size = new System.Drawing.Size(119, 20);
+            this.txtepisodio.TabIndex = 6;
+            // 
+            // btnClose
+            // 
+            this.btnClose.BackColor = System.Drawing.Color.Red;
+            this.btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClose.Location = new System.Drawing.Point(760, 12);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(28, 26);
+            this.btnClose.TabIndex = 7;
+            this.btnClose.Text = "X";
+            this.btnClose.UseVisualStyleBackColor = false;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // boxgenero
+            // 
+            this.boxgenero.FormattingEnabled = true;
+            this.boxgenero.Items.AddRange(new object[] {
+            "Ação",
+            "Luta"});
+            this.boxgenero.Location = new System.Drawing.Point(101, 226);
+            this.boxgenero.Name = "boxgenero";
+            this.boxgenero.Size = new System.Drawing.Size(121, 21);
+            this.boxgenero.TabIndex = 8;
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(103, 133);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(119, 20);
+            this.txtId.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Navy;
+            this.label1.Font = new System.Drawing.Font("Monotype Corsiva", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(33, 136);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 18);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "ID";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Navy;
+            this.label2.Font = new System.Drawing.Font("Monotype Corsiva", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(24, 175);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 15);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "ANIME";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Navy;
+            this.label3.Font = new System.Drawing.Font("Monotype Corsiva", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(24, 228);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 15);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "GENERO";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Navy;
+            this.label4.Font = new System.Drawing.Font("Monotype Corsiva", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(24, 287);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(72, 15);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "EPISÓDIO";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.GridColor = System.Drawing.Color.Black;
+            this.dataGridView1.Location = new System.Drawing.Point(279, 75);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.dataGridView1.Size = new System.Drawing.Size(490, 351);
+            this.dataGridView1.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Catalogo_de_Animes.Properties.Resources.naruto_and_teammates_fan_art_dbt18qjb7b1cstr1;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.boxgenero);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.txtepisodio);
+            this.Controls.Add(this.txtanime);
+            this.Controls.Add(this.btndelete);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnAdd);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btndelete;
+        private System.Windows.Forms.TextBox txtanime;
+        private System.Windows.Forms.TextBox txtepisodio;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.ComboBox boxgenero;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ Catalogo de Animes/Form1.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace Catalogo_de_Animes
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            AtualizarDataGridView();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (SqlConnection con = new SqlConnection("Data Source=SETH;Initial Catalog=Animes;Integrated Security=True"))
+                {
+                    con.Open();
+
+                    string query = "INSERT INTO Anime ( Id, Anime, Genero, Episodio) VALUES ( @Id, @Anime, @Genero, @Episodio)";
+                    SqlCommand command = new SqlCommand(query, con);
+
+                    command.Parameters.AddWithValue("@Id", int.Parse(txtId.Text));
+                    command.Parameters.AddWithValue("@Anime", txtanime.Text);
+                    command.Parameters.AddWithValue("@Genero", boxgenero.Text);
+                    command.Parameters.AddWithValue("@Episodio", txtepisodio.Text);
+
+                    command.ExecuteNonQuery();
+                    MessageBox.Show("Adicionado com Sucesso");
+                    AtualizarDataGridView();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro ao adicionar: " + ex.Message);
+            }
+        }
+
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void btndelete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (dataGridView1.SelectedRows.Count > 0)
+                {
+                    int selectedId = Convert.ToInt32(dataGridView1.SelectedRows[0].Cells["ID"].Value);
+
+                    using (SqlConnection con = new SqlConnection("Data Source=SETH;Initial Catalog=Animes;Integrated Security=True"))
+                    {
+                        con.Open();
+
+                        string query = "DELETE FROM Anime WHERE Id = @Id";
+                        SqlCommand command = new SqlCommand(query, con);
+                        command.Parameters.AddWithValue("@Id", selectedId);
+
+                        int rowsAffected = command.ExecuteNonQuery();
+
+                        if (rowsAffected > 0)
+                        {
+                            MessageBox.Show("Registro excluído com sucesso!");
+
+                            AtualizarDataGridView();
+                        }
+                        else
+                        {
+                            MessageBox.Show("Nenhum registro foi excluído.");
+                        }
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Selecione um registro para excluir.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro ao excluir o registro: " + ex.Message);
+            }
+        }
+
+ 
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (dataGridView1.SelectedRows.Count > 0)
+                {
+                    int selectedId = Convert.ToInt32(dataGridView1.SelectedRows[0].Cells["ID"].Value);
+
+
+                    string novoAnime = txtanime.Text;
+                    string novoGenero = boxgenero.Text;
+                    string novoEpisodio = txtepisodio.Text;
+
+                    using (SqlConnection con = new SqlConnection("Data Source=SETH;Initial Catalog=Animes;Integrated Security=True"))
+                    {
+                        con.Open();
+
+                        string query = "UPDATE Anime SET Anime = @Anime, Genero = @Genero, Episodio = @Episodio WHERE Id = @Id";
+                        SqlCommand command = new SqlCommand(query, con);
+
+                        command.Parameters.AddWithValue("@Id", selectedId);
+                        command.Parameters.AddWithValue("@Anime", novoAnime);
+                        command.Parameters.AddWithValue("@Genero", novoGenero);
+                        command.Parameters.AddWithValue("@Episodio", novoEpisodio);
+
+                        int rowsAffected = command.ExecuteNonQuery();
+
+                        if (rowsAffected > 0)
+                        {
+                            MessageBox.Show("Registro atualizado com sucesso!");
+
+                            AtualizarDataGridView();
+                        }
+                        else
+                        {
+                            MessageBox.Show("Nenhum registro foi atualizado.");
+                        }
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Selecione um registro para editar.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro ao editar o registro: " + ex.Message);
+            }
+        }
+
+        private void AtualizarDataGridView()
+        {
+            try
+            {
+                using (SqlConnection con = new SqlConnection("Data Source=SETH;Initial Catalog=Animes;Integrated Security=True"))
+                {
+                    con.Open();
+
+                    string query = "SELECT * FROM Anime";
+                    SqlCommand command = new SqlCommand(query, con);
+
+                    DataTable dataTable = new DataTable();
+
+                    SqlDataAdapter adapter = new SqlDataAdapter(command);
+                    adapter.Fill(dataTable);
+
+                    dataGridView1.DataSource = dataTable;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro ao atualizar o DataGridView: " + ex.Message);
+            }
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
