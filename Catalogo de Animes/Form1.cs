0b830375dde187220fc8b11412896bec900f01bb
jhonnyseth <jhonatan.mallet@gmail.com>
2023-10-30 05:49:03 -0300

    atualizar

--- Catalogo de Animes/Form1.cs
+++ Catalogo de Animes/Form1.cs
@@ -3,12 +3,10 @@ using System.Data;
 using System.Data.SqlClient;
 using System.Windows.Forms;
 
-
 namespace Catalogo_de_Animes
 {
     public partial class Form1 : Form
     {
-
         public Form1()
         {
             InitializeComponent();
